a25e078722f5c7228d37a12dc3801ea627184885
ivan knapp <i.knapp@alfacapital.ru>
2023-10-22 13:51:37 +0300

    init

---
+++ src/main/java/ru/knapp/WebHelloServlet.java
@@ -0,0 +1,20 @@
+package ru.knapp;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/web")
+public class WebHelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        response.getWriter().println("<h1>Hello, java web servlet!</h1>");
+    }
+}
